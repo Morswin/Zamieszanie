ee24112eb0cbfccdb423703c186855bf191b2326
Morswin <41349347+Morswin@users.noreply.github.com>
2024-02-25 01:08:07 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-navigation-drawer rail>
+        <v-avatar v-for="n in 5" :key="n" class="ma-2"><v-img src="https://picsum.photos/100/100?random"></v-img></v-avatar>
+      </v-navigation-drawer>
+      <v-navigation-drawer>Użytkownicy/kanały</v-navigation-drawer>
+      <v-card class="h-screen">
+        <v-app-bar>Podstawowe interakcje</v-app-bar>
+        Czat
+        <v-footer app>Input</v-footer>
+        <v-navigation-drawer location="right">Info o osobie/aktywni użytkownicy serwera</v-navigation-drawer>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data () {
-    return {
-      //
-    }
-  },
-})
+<script lang="ts" setup>
 </script>
